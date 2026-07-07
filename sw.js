19709db7d51f2f7b9b4970bc2acd1398e597424b
Overlandslabs <slabs@duck.com>
2026-07-07 05:13:39 +0200

    commit

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // falls back to the library index, never to an unrelated page.
 // Bump CACHE (v1 -> v2 ...) whenever library files change to force refresh.
 
-const CACHE = 'lc76-library-v13';
+const CACHE = 'lc76-library-v14';
 
 const PRECACHE = [
   './',
@@ -66,6 +66,7 @@ const PRECACHE = [
   './Namibia_Overland_Profile_2026-04.html',
   './Tanzania_Overland_Profile_2026-05.html',
   './Mozambique_Overland_Profile_2026-07.html',
+  './Malawi_Overland_Profile_2026-07.html',
   './Zambia_Overland_Profile_2026-04.html',
 ];
 
