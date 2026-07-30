36703532c24b9c1095fba0d3079b0a24760a1bd1
Overlandslabs <slabs@duck.com>
2026-07-30 12:06:40 +0200

    commit

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
 // but any tiles fetched successfully while online are cached opportunistically
 // for later offline reuse via the same runtime fetch handler.
 
-const CACHE = 'lc76-library-v25';
+const CACHE = 'lc76-library-v26';
 
 const CDN_HOSTS = [
   'cdnjs.cloudflare.com',
@@ -65,6 +65,7 @@ const PRECACHE = [
   './LC76_Health_Hygiene_Guide.html',
   './LC76_History_of_Overlanding.html',
   './LC76_How_To_Use_This_Library.html',
+  './LC76_Open_Items_Dashboard.html',
   './LC76_Insurance_Medevac.html',
   './LC76_Kalahari_Profile_2026-04.html',
   './LC76_Karoo_Winter_Loop_2026.html',
